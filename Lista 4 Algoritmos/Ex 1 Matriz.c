324e3f4709d4c6f014fab793f24b2cd492cff470
MauricioNobrega <131412612+MauricioNobrega22@users.noreply.github.com>
2024-06-01 14:14:12 -0300

    Ex 1 Matriz.c

---
+++ Lista 4 Algoritmos/Ex 1 Matriz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main (){
+	
+	int i, j, mat[3][3];
+	
+	printf("Informe os elementos da matriz: \n");
+	
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			scanf("%d", &mat[i][j]);
+		}
+	}
+	
+	printf("A matriz ficou dessa forma: \n");
+	
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("[%d] ", mat[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
